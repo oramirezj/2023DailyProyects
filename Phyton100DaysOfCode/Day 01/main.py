02e5281e13048cf1c8184f90d08ba5ff651d5291
Omar Ramírez <lic.omar.r.j@gmail.com>
2023-02-01 21:00:05 -0600

    Day 1 curse 100 of code using phyton by Angela Yu

---
+++ Phyton100DaysOfCode/Day 01/main.py
@@ -0,0 +1,8 @@
+# Program to generate Band Name, ask city you grew up in, pets name and make a band name with those data.
+print('Welcome to the Band Name Generator')
+# Ask the city you grew up and save on variable cityGrowUp
+cityGrowUp = input("What's the city you grew up in? \n")
+# Ask pet's name and save on variable petsName
+petsName = input("What's your pet's name? \n")
+# Print the name of the band made from 2 previus variables
+print("Your band name could be:", cityGrowUp, petsName)
